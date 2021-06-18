ce3881a86c33a47fe4bd781b2ce0c71e9a6932f6
Anushka Eranga <aer@dipscloud.com>
2021-06-18 13:50:56 +0530

    css added for type

--- src/pages/type.js
+++ src/pages/type.js
@@ -1,9 +1,34 @@
-import React from 'react'
-
+import React from 'react';
+import './type.css';
 function type() {
     return (
-        <div>
-            <h>type page loaded</h>
+        <div className ="type">
+        <form>
+            <label >DocumentType ID</label>
+            <span><input></input></span>
+            <br/>
+            <label >In Use</label>
+            <span><input type="checkbox"/> </span>
+            <br/>
+            <label >Selected ERP groups</label>
+            <span>  
+                <select name="erpGroup" id="erp">
+                    <option value="erp1">erp1</option>
+                    <option value="erp2">erp2</option>
+                    <option value="erp3">erp3</option>
+                    <option value="erp4">erp4</option>
+                </select>
+            </span>
+            <br/>
+            <br/>
+            <label >DocumentType ID</label>
+            <span><input></input></span>
+            <br/>
+            <label >DocumentType ID</label>
+            <span><input></input></span>
+            <br/>
+            <button type="submit" value="Submit">Submit</button>
+        </form>
         </div>
     )
 }
